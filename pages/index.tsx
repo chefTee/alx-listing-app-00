da4c444b1ab31e064278167ac8281296f27228c3
chefTee <taiwosharon36@gmail.com>
2025-06-21 00:48:32 +0100

    Renaming File

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your sharon
+            Trying to make Changes and update on Github
           </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
